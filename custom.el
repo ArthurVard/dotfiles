c222bbc37e1820ef3f52e8825a8a0fb90f3c694c
Arthur Vardanyan <artie.vard@gmail.com>
2019-04-07 18:04:06 +0400

    configure to seperate custom configs lines to another file.

---
+++ custom.el
@@ -0,0 +1,41 @@
+;; =============================================================================
+;; Custom set variables section
+;; =============================================================================
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(comint-completion-addsuffix t)
+ '(comint-completion-autolist t)
+ '(comint-input-ignoredups t)
+ '(comint-move-point-for-output t)
+ '(comint-scroll-show-maximum-output t)
+ '(comint-scroll-to-bottom-on-input t)
+ '(custom-enabled-themes (quote (tsdh-dark)))
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(haskell-stylish-on-save t)
+ '(org-agenda-files
+   (quote
+    ("~/orgs/another-page.org" "~/documents/org/index.org")))
+ '(package-selected-packages
+   (quote
+    (engine-mode org-bullets-mode graphviz-dot-mode gnuplot org-bullets diff-hl nix-mode all-the-icons-dired dired-sidebar smex smartparens ido-vertical-mode ido-yes-or-no use-package)))
+ '(safe-local-variable-values
+   (quote
+    ((haskell-indent-spaces . 4)
+     (haskell-indent-spaces . 2)
+     (hindent-style . "chris-done")
+     (hindent-style . "gibiansky")
+     (hindent-style . "johan-tibell")
+     (haskell-process-type . cabal-repl)
+     (shm-lambda-indent-style . leftmost-parent)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
